2d6e8c3ad97330fe81a0c378f6f3504bd94b2838
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-04-02 20:46:01 +0300

    add user form

---
+++ src/app/(protected)/add-user/add-user-form/AddUserForm.tsx
@@ -0,0 +1,87 @@
+
+'use client'
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function AddUserForm() {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        
+        <CardTitle>Създаване на потребител</CardTitle>
+        
+        <CardDescription>
+           {/* TODO Error Handling */}
+        </CardDescription>
+
+      </CardHeader>
+
+      <CardContent>
+        <form onSubmit={submitHandler}>
+          <div className="flex flex-col gap-6">
+
+            <div className="grid gap-2">
+              <Label htmlFor="username">Потребителско име</Label>
+              <Input
+                id="username"
+                type="text"
+                placeholder="Вашето потребителско име"
+                onChange={(e)=>setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <Label htmlFor="email">Имейл</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Парола</Label>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Вашата парола"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+             <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="confirmPassword">Потвърдете паролата</Label>
+              </div>
+              <Input
+                id="confirmPassword"
+                type="password"
+                placeholder="Потвърдете вашата парола"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <Button className="w-full mt-6" type="submit">Добави потребител</Button>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/(protected)/add-user/page.tsx
@@ -0,0 +1,10 @@
+import { AddUserForm } from "./add-user-form/AddUserForm";
+
+export default function AddUserPage() {
+
+    return (
+        <div className="min-h-screen flex items-center justify-center flex-col bg-gray-100">
+            <AddUserForm/>
+        </div>
+    )
+}
